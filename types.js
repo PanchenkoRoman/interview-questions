45a999b5c9fd7fdfd04ad56b1bee44f60004b783
Roman Panchenko <r.panchenko21@gmail.com>
2021-03-05 15:35:44 +0200

    Update types.js

--- types.js
+++ types.js
@@ -1,11 +1,11 @@
 let arr = [1, 2];
-let brr = a;
+let brr = arr;
 brr = [42, 43];
 arr[0]; // ???
 
  /////////////////
  
 let arr = [1, 2];
-let brr = a;
+let brr = arr;
 brr[0] = 42;
 arr[0]; // ???
